3f246ff1e4c6f25378578c29fe965ee2f2269497
unknown <army.polk@gmail.com>
2021-10-29 12:20:39 -0700

    Meter_Feet_Converter

---
+++ AndroidStudioProjects/MeterConverter/app/src/main/java/com/example/meterconverter/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.meterconverter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+       findViewById(R.id.editTextNumber2);
+
+       Button calculateButton =  findViewById(R.id.button_calaculate);
+
+       EditText resultText2 = findViewById(R.id.editTextNumber2);
+       EditText resultText1 = findViewById(R.id.editTextNumber);
+
+       calculateButton.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View view) {
+
+               Toast.makeText(context: MainActivity.this, text: "Your conversion is complete!", Toast.LENGTH_LONG){
+
+               }
+           }
+
+       });
+    }
+ }
